ef242a225dae07c902c9462d5dc4a30d86dfad03
aj <sergioajini@gmail.com>
2020-05-12 11:49:02 -0400

    pad checkpoint name with zeros

--- utils.py
+++ utils.py
@@ -4,17 +4,17 @@ import torch
 def save_checkpoint(model, criterion, optimizer, epoch):
     torch.save(
         model.state_dict(),
-        f'checkpoints/model_{epoch+1:03d}.pt'
+        f'checkpoints/model_{epoch+1:05d}.pt'
     )
     if criterion:
         torch.save(
             criterion.state_dict(),
-            f'checkpoints/criterion_{epoch+1:03d}.pt'
+            f'checkpoints/criterion_{epoch+1:05d}.pt'
         )
     if optimizer:
         torch.save(
             optimizer.state_dict(),
-            f'checkpoints/optimizer_{epoch+1:03d}.pt'
+            f'checkpoints/optimizer_{epoch+1:05d}.pt'
         )
 
 
