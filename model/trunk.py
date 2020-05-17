5a64ec735b6d7e13ebeaf521112ed0829ad35e52
aj <sergioajini@gmail.com>
2020-05-17 09:10:55 -0400

    standerd ResNet

--- model/trunk.py
+++ model/trunk.py
@@ -127,7 +127,7 @@ class ResNet(Trunk):
             norm_layer = nn.BatchNorm2d
         self._norm_layer = norm_layer
 
-        self.inplanes = 32
+        self.inplanes = 64
         self.dilation = 1
         if replace_stride_with_dilation is None:
             # each element in the tuple indicates if we should replace
@@ -153,24 +153,24 @@ class ResNet(Trunk):
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.LeakyReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 32, layers[0])
+        self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(
             block,
-            64,
+            128,
             layers[1],
             stride=2,
             dilate=replace_stride_with_dilation[0]
         )
         self.layer3 = self._make_layer(
             block,
-            128,
+            256,
             layers[2],
             stride=2,
             dilate=replace_stride_with_dilation[1]
         )
         self.layer4 = self._make_layer(
             block,
-            256,
+            512,
             layers[3],
             stride=2,
             dilate=replace_stride_with_dilation[2]
@@ -256,4 +256,4 @@ class ResNet(Trunk):
 if __name__ == '__main__':
     x = torch.rand((4, 1, 40, 200))
     model = ResNet()
-    print(model(x).size())
+    y = model(x)
