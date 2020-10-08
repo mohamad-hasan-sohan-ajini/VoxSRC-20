36ea101cfe0829921b42010411fb9ae1260cc1aa
hajix <sergioajini@gmail.com>
2020-10-08 19:33:31 -0400

    add data augmentation

--- data_loader.py
+++ data_loader.py
@@ -3,11 +3,13 @@ import csv
 from collections import defaultdict
 import random
 import json
+from glob import glob
 
 import torch
 import torch.nn.functional as F
 import librosa
 import numpy as np
+from scipy import signal
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 from tinytag import TinyTag
@@ -22,7 +24,10 @@ class FeatureExtractor(object):
             n_frames,
             n_fft,
             n_filterbanks,
-            feat_type
+            feat_type,
+            musan_path,
+            rir_path,
+            augmet_prob
     ):
         self.sample_rate = sample_rate
         self.n_win_length = int(win_length * sample_rate)
@@ -31,8 +36,31 @@ class FeatureExtractor(object):
         self.n_fft = n_fft
         self.n_filterbanks = n_filterbanks
         self.feat_type = feat_type
+        self.add_musan = musan_path
+        self.add_rir = rir_path
+        self.augmet_prob = augmet_prob
 
         self.clip_length = (n_frames-3) * self.n_hop_length + self.n_win_length
+
+        # init augmentation stuff
+        if musan_path:
+            self.noise_snr_range = {
+                'noise': (0, 15),
+                'speech': (15, 20),
+                'music': (5, 15)
+            }
+            self.num_noise = {
+                'noise': (1, 1),
+                'speech': (3, 7),
+                'music': (1, 1)
+            }
+            self.musan_files  = defaultdict(list)
+            for filepath in glob(os.path.join(musan_path, '*/*/*.wav')):
+                noise_cat = filepath.split('/')[-3]
+                self.musan_files[noise_cat].append(filepath)
+
+        if rir_path:
+            self.rir_files = glob(os.path.join(rir_path, '*/*/*.wav'))
 
     def load_audio_4train(self, filepath, filesize):
         if filesize >= self.clip_length:
@@ -48,7 +76,49 @@ class FeatureExtractor(object):
             # load whole audio and fix length
             x, _ = librosa.load(filepath, sr=None)
             x = self.fix_length(x)
+
+        # augmentation
+        if random.random() < self.augmet_prob:
+            noise_type = random.randint(0, 3)
+            if noise_type == 0:
+                rir_file = random.choice(self.rir_files)
+                rir, _ = librosa.load(rir_file, sr=self.sample_rate)
+                rir /= np.sqrt(np.sum(rir ** 2))
+                x = signal.convolve(x, rir, mode='same')
+            elif noise_type == 1:
+                x = self.add_noise(x, 'music')
+            elif noise_type == 2:
+                x = self.add_noise(x, 'speech')
+            elif noise_type == 3:
+                x = self.add_noise(x, 'noise')
+
         return self.audio_to_feat(x)
+
+    def add_noise(self, x, noise_cat):
+        x_db = 10 * np.log10(np.mean(x ** 2) + 1e-4)
+        num_noise = self.num_noise[noise_cat]
+        file_list = random.sample(
+            self.musan_files[noise_cat],
+            random.randint(*num_noise)
+        )
+        for filepath in file_list:
+            # load noise
+            noise, _ = librosa.load(filepath, sr=self.sample_rate)
+            filesize = noise.shape[-1]
+            if filesize >= self.clip_length:
+                offset = np.random.randint(
+                    0,
+                    max(0, filesize - self.clip_length)
+                )
+                noise = noise[offset:offset + self.clip_length]
+            else:
+                noise = self.fix_length(noise)
+            # scale and add to original signal
+            noise_snr = random.uniform(*self.noise_snr_range[noise_cat])
+            noise_db = 10 * np.log10(np.mean(noise ** 2) + 1e-4)
+            noise *= np.sqrt(10 ** ((x_db - noise_db - noise_snr) / 10))
+            x += noise
+        return x
 
     def load_audio_4test(self, filepath):
         x, _ = librosa.load(filepath, sr=None)
@@ -105,6 +175,9 @@ class VoxCelebDataset(Dataset):
             n_fft,
             n_filterbanks,
             feat_type,
+            musan_path,
+            rir_path,
+            augmet_prob,
             mode,
             csv_path,
             samples_per_speaker
@@ -116,7 +189,10 @@ class VoxCelebDataset(Dataset):
             n_frames,
             n_fft,
             n_filterbanks,
-            feat_type
+            feat_type,
+            musan_path,
+            rir_path,
+            augmet_prob
         )
         self.mode = mode
 
@@ -188,7 +264,6 @@ class VoxCelebDataset(Dataset):
 
 
 if __name__ == '__main__':
-    VoxCelebDataset()
     ds = VoxCelebDataset(
         16000,
         .025,
@@ -196,13 +271,16 @@ if __name__ == '__main__':
         200,
         512,
         40,
-        'mel'
-        'train',
-        'data/train.csv',
+        'spect',
+        '/data/musan',
+        '/data/RIRS_NOISES/simulated_rirs',
+        .8,
+        'dev',
+        '/data/voxceleb2_dev.csv',
         3
     )
 
-    dl = DataLoader(ds, batch_size=4, shuffle=True, num_workers=4, drop_last=True)
+    dl = DataLoader(ds, batch_size=4, shuffle=True, num_workers=8, drop_last=True)
     for x, y in tqdm(dl):
         pass
     print(x)
